84383f8bf09ce5c44616ca9b2539a0ea21b7ee30
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-04-08 17:35:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,54 @@ import React, { useReducer, useEffect, useState } from "react";
 const initialState = { hotels: [], filteredHotels: [] };
 
 function reducer(state, action) {
-  switch (action) {
+  switch (action.type) {
     case "FETCH_SUCCESS":
-     
+      return {...state, hotels: action.payload}
     case "FILTER":
-     
+      return {...state, filteredHotels: state.hotels.filter((hotel)=>{
+        console.log(hotel.city);
+        console.log(action.payload)
+        return hotel.city.toLowerCase().includes(action.payload.toLowerCase())
+      })}
     default:
+      return state
    
   }
 }
 export default function Home() {
+  const [city, setCity] = useState('');
    const [state, dispatch] = useReducer(reducer, initialState);
- 
 
-
+  useEffect(()=>{
+    fetch('https://content.newtonschool.co/v1/pr/63b85bcf735f93791e09caf4/hotels')
+      .then((response)=>{
+        return response.json()
+      })
+      .then((data)=>{
+        console.log(data)
+        dispatch({ type: "FETCH_SUCCESS", payload: data});
+      })
+  },[])
 
   useEffect(() => {
-    dispatch({ type: "FILTER"});
-  });
+    console.log('called')
+    dispatch({ type: "FILTER", payload: city});
+  }, [city]);
 
   return (
     <div className="App">
       <input
         type="text"
         placeholder="Enter city name"
+        onChange={(e)=>setCity(e.target.value)}
+        value={city}
       />
-     
-        <p >hotel_name</p>
+      {state.filteredHotels.map((hotel)=>{
+        return (
+          <p key={`${hotel.city}-${hotel.hotel_name}`}>{hotel.hotel_name}</p>
+        )
+      })}
+        
      
     </div>
   );
